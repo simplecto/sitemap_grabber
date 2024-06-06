a0664067165aeaf06dbeef3061b0ae1641f20b52
Developer <dev@simplecto.com>
2024-06-06 14:41:48 +0200

    more module hell

---
+++ sitemap_grabber/__init__.py
@@ -0,0 +1,4 @@
+from .sitemap_grabber import SitemapGrabber
+from .well_known_files import WellKnownFiles, WellKnownFilesException
+
+__all__ = ["SitemapGrabber", "WellKnownFiles", "WellKnownFilesException"]
